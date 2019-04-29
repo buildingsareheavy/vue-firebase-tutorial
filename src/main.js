a15c411d2aa3ab0212ae09c370f935147f83b4ef
Ethan Barrett <hello@buildingsareheavy.com>
2019-04-29 15:48:28 -0600

    added the login page and set up vue-router in the files, and added views folder

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: "history",
-  routes: []
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    }
+  ]
 });
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button>Connection</button>
+    <p>You don't have an account? You can create one</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  margin-top: 40px;
+}
+
+input {
+  margin: 10px auto;
+  width: 30%;
+  padding: 15px;
+  display: block;
+}
+
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 1rem;
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+}
+</style>
